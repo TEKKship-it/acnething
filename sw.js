f8c561eb478c28e899c8ac7dc0ac2ef140946d3a
TEKKship-it <tekkerrss@gmail.com>
2026-09-13 15:22:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "skinlog-v45";
+var CACHE = "skinlog-v48";
 var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
